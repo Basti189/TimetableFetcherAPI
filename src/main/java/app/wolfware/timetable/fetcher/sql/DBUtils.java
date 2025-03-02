f0a567f4bc72a583f292e787f8268b4f6e1e8c89
Sebastian Wolf <sebastian@wolfware.app>
2025-03-02 21:57:42 +0100

    Add transition to response

--- src/main/java/app/wolfware/timetable/fetcher/sql/DBUtils.java
+++ src/main/java/app/wolfware/timetable/fetcher/sql/DBUtils.java
@@ -13,8 +13,8 @@ import java.util.Map;
 
 public class DBUtils {
 
-    private final static String select_train = "SELECT t.id, t.number, j.position, s.name, s.alias, j.arrival_pt, j.arrival_pp, j.departure_pt, j.departure_pp, art.number AS 'arrival_number', art.id AS 'arrival_id', dt.number AS 'departure_number', dt.id AS 'departure_id' FROM train t left join journey j on j.id = t.id LEFT JOIN station s ON s.id = j.station LEFT JOIN train art ON art.id = j.arrival_wings LEFT JOIN train dt ON dt.id = j.departure_wings WHERE t.number = ? AND t.id like ? ORDER BY j.position ASC";
-    private final static String select_additional_info = "SELECT * from additional_info WHERE id = ?";
+    private final static String select_train = "SELECT t.id, t.number, t.origin, t.destination, j.position, s.name, s.alias, j.arrival_pt, j.arrival_pp, j.departure_pt, j.departure_pp, art.number AS 'arrival_number', art.id AS 'arrival_id', dt.number AS 'departure_number', dt.id AS 'departure_id', tt.id AS 'transition_id', tt.number AS 'transition_number', tt.timestamp AS 'transition_timestamp', tt.origin AS 'transition_origin', tt.destination AS 'transition_destination' FROM train t left join journey j on j.id = t.id LEFT JOIN station s ON s.id = j.station LEFT JOIN train art ON art.id = j.arrival_wings LEFT JOIN train dt ON dt.id = j.departure_wings LEFT JOIN train tt on tt.id = j.transition WHERE t.number = ? AND t.id like ? ORDER BY j.position ASC";
+    private final static String select_additional_info = "SELECT * from additional_train_info WHERE id = ?";
     private final static DateTimeFormatter formatterDateTime = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
 
     public static String selectTrains(int train, String date) {
@@ -31,12 +31,16 @@ public class DBUtils {
             pstmt.setString(2, "%-" + date + "%");
 
             int number = -1;
+            String origin = null;
+            String destination = null;
 
             ResultSet rs = pstmt.executeQuery();
             while (rs.next()) {
                 if (number == -1) {
                     number = rs.getInt("number");
                     id = rs.getString("id");
+                    origin = rs.getString("origin");
+                    destination = rs.getString("destination");
                     foundTrain = true;
                 }
                 Map<String, Object> stationData = new HashMap<>();
@@ -64,9 +68,25 @@ public class DBUtils {
                 stationData.put("wings_arrival_number", rs.getString("arrival_number"));
                 //stationData.put("wings_departure_id", rs.getString("departure_id"));
                 stationData.put("wings_departure_number", rs.getString("departure_number"));
+                String transitionId = rs.getString("transition_id");
+                String transitionNumber = rs.getString("transition_number");
+                Timestamp transitionTimestamp = rs.getTimestamp("transition_timestamp");
+                String transitionOrigin = rs.getString("transition_origin");
+                String transitionDestination = rs.getString("transition_destination");
+                if (transitionId != null) {
+                    Map<String, String> transitionData = new HashMap<>();
+                    transitionData.put("id", transitionId);
+                    transitionData.put("number", transitionNumber);
+                    transitionData.put("timestamp", formatterDateTime.format(transitionTimestamp.toLocalDateTime()));
+                    transitionData.put("origin", transitionOrigin);
+                    transitionData.put("destination", transitionDestination);
+                    stationData.put("transition", transitionData);
+                }
                 stations.add(stationData);
             }
             resultMap.put("stations", stations);
+            resultMap.put("origin", origin);
+            resultMap.put("destination", destination);
             resultMap.put("number", number);
             resultMap.put("id", id);
         } catch (SQLException e) {
